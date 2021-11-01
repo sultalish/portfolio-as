4894c205dcfa562eb5cba1deb36585f458b7342e
sultalish <a-sultangazin@knights.ucf.edu>
2021-11-01 00:57:09 -0400

    Create a project file
    Implement Projects, About views
    Create main view

---
+++ src/data.js
@@ -0,0 +1,11 @@
+export const projects = [
+    {
+        title: "SimpleList Scanner",
+        subtitle: "HTML/CSS/JS",
+        description: "SimpleList Scanner is the project that let's users connect any Scanner to their account using the Raspberry Pi. It let's them scan items and add them to their Shopping List. While using the app, the user will recieve notifications and suggestions on the products he/she should buy based on the products he added before. Also, scanner could be used to delete the product from the list",
+        image: "./simplelist.jpg",
+        link: "https://cop4331-project.web.app/",
+    },
+];
+
+export default projects
